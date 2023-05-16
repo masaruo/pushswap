0f3e117c462f3d67febb7fda1c2ff9d00e943b00
Masaru Ogawa <mogawa@c1r9s1.42tokyo.jp>
2023-05-16 11:35:05 +0900

    basic radix sort working

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 11:20:39 by mogawa            #+#    #+#             */
-/*   Updated: 2023/05/15 21:14:14 by mogawa           ###   ########.fr       */
+/*   Updated: 2023/05/16 10:55:52 by mogawa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,29 +35,26 @@ int	main(int argc, char **argv)
 		int i = 0;
 		while (i < stk.size)
 			ft_printf("%d\n", stk.stkf[i++]);//!
-		ft_printf("\n========\n");//!
-
-
-
-		pb(&stk);
-		pb(&stk);
-		pb(&stk);
+		// pb(&stk);
+		// pb(&stk);
+		// pb(&stk);
 		// sb(&stk);
 		// pa(&stk);
 		// pa(&stk);
 		// sb(&stk);
-		sa(&stk);
-		sb(&stk);
-		rb(&stk);
-		rb(&stk);
-		rrb(&stk);
-		rrb(&stk);
-		pa(&stk);
-		pa(&stk);
-		ra(&stk);
-		ra(&stk);
-		rra(&stk);
-		ft_printf("====after op====\n");
+		// sa(&stk);
+		// sb(&stk);
+		// rb(&stk);
+		// rb(&stk);
+		// rrb(&stk);
+		// rrb(&stk);
+		// pa(&stk);
+		// pa(&stk);
+		// ra(&stk);
+		// ra(&stk);
+		// rra(&stk);
+		ft_radix_sort(&stk);
+		ft_printf("====after radix sort ====\n");
 		i = 0;
 		while (i < stk.size)
 			ft_printf("%d\n", stk.stkf[i++]);//!
--- srcs/push_swap_algo.c
+++ srcs/push_swap_algo.c
@@ -6,26 +6,35 @@
 /*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 14:22:31 by mogawa            #+#    #+#             */
-/*   Updated: 2023/05/15 21:19:32 by mogawa           ###   ########.fr       */
+/*   Updated: 2023/05/16 11:18:37 by mogawa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/*
+*/
 void	ft_radix_sort(t_stk *stk)
 {
 	size_t	i;
 	size_t	j;
 
-	i = 0;
 	j = 0;
 	while (j < 32)
 	{
+		i = 0;
 		while (i < stk->size)
 		{
-			if ((stk[i] >> j & 1) == 0)
-				// pb(stk, a0ptr, size);
+			if ((stk->stkf[stk->slit] >> j & 1) == 0)
+				pb(stk);
+			else
+				ra(stk);
 			i++;
 		}
+		while (0 < stk->slit)
+		{
+			pa(stk);
+		}
+		j++;
 	}
 }
